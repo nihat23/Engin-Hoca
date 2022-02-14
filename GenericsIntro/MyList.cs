ebf563f10e2352e1a8a21316d4730460770c5b20
Nihat BEYI <nihatbeyi@hotmail.com>
2022-02-14 19:52:15 +0300

    Temel Egitim

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+
+        public MyList()//MyList newlerden otomatık calısır..
+        {
+            items = new T[0];//bir class newlendıgınde calısan bloga contructor denır..
+        }
+        public void Add(T item)
+        {
+            items = new T[items.Length + 1];
+        }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            // isimler.Add("engin");
+
+
+
+            List<string> isimler2 = new List<string>();
+            Console.WriteLine(isimler2.Count);
+
+           // Console.WriteLine(isimler);
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
